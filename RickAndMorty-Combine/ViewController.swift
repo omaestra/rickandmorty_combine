51d27db5c5c8cfc9f7db047f5279d42d875ad6f4
omaestra <om@visionarity.com>
2021-06-16 13:12:14 +0200

    Initial Commit

---
+++ RickAndMorty-Combine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty-Combine
+//
+//  Created by omaestra on 16/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
